18afc0659cf28523abcabef5ec4b27e6fa297f10
nimfersus <105855729+nimfersus@users.noreply.github.com>
2022-05-19 16:33:15 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
+        basic.pause(5000)
         basic.setLedColor(0xffff00)
-        basic.pause(1500)
+        basic.pause(3000)
         basic.setLedColor(0xff0000)
-        basic.pause(9000)
         radio.sendNumber(2)
     }
     if (receivedNumber == 3) {
@@ -12,6 +12,3 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 radio.setGroup(74)
 basic.setLedColor(0x00ff00)
-basic.forever(function () {
-	
-})
